798ccd2d7298fecc14f95f5dfb9a53c3ee7ee51d
Chathu Vishwajith <4evercreations@gmail.com>
2018-03-25 12:33:23 +0800

    Add config.example file

---
+++ config/config.example.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//Rename this as config.dev.js with your deatils
+
+module.exports = {
+    sessionSecret: process.env.SESSION_SECRET,
+    sessionExpiry: process.env.JWT_TOKEN_EXPIRE_TIME || 1200,
+    db: {
+        uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/api-demo',
+        // Enable mongoose debug mode
+        debug: process.env.MONGODB_DEBUG || false
+    },
+    seedDB: {
+        seed: process.env.MONGO_SEED === 'true'
+    }
+};
